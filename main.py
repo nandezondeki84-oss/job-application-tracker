9cd5e1353e782f76c615f1cbf926cb5695622f6a
nandezondeki84-oss <nandezondeki84@gmail.com>
2026-08-11 22:10:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from database import create_database
+from jobs import add_application
 
 create_database()
 
 print("Welcome to the Job Application Tracker!")
-print("Database created successfully.")
+
+company = input("Company name: ")
+position = input("Job title: ")
+status = input("Application status: ")
+date_applied = input("Date applied: ")
+
+add_application(company, position, status, date_applied)
+
+print("Application saved successfully!")
