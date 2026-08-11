6e9ab06f70cd673e62e3e158c1f831232466a0ad
nandezondeki84-oss <nandezondeki84@gmail.com>
2026-08-11 22:12:18 +0200

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -12,3 +12,19 @@ def add_application(company, position, status, date_applied):
 
     connection.commit()
     connection.close()
+
+
+def get_applications():
+    connection = sqlite3.connect("jobs.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, company, position, status, date_applied
+        FROM applications
+        ORDER BY id DESC
+    """)
+
+    applications = cursor.fetchall()
+    connection.close()
+
+    return applications
